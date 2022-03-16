e335e28ba85257fdc098e956fe0586ab38ab379b
ReneDeicker <rene.deicker1105@gmail.com>
2022-03-16 20:36:51 +0100

    first code parts, gallery, apartmentdescription

---
+++ HausAsteri/src/app/apartment-description/apartment-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-description',
+  templateUrl: './apartment-description.component.html',
+  styleUrls: ['./apartment-description.component.scss']
+})
+export class ApartmentDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- HausAsteri/src/app/app.module.ts
+++ HausAsteri/src/app/app.module.ts
@@ -3,16 +3,67 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {ButtonModule} from 'primeng/button';
+import {GalleriaModule} from 'primeng/galleria';
+
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ImageViewComponent } from './image-view/image-view.component';
+import { Globals } from './globals/global';
+import { ApartmentDescriptionComponent } from './apartment-description/apartment-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageViewComponent,
+    ApartmentDescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    NoopAnimationsModule,
+
+    ButtonModule,
+    GalleriaModule,
+
+    MatCardModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    Globals],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
